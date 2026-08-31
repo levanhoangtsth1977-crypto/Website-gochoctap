19b0923e571c03a93ff0a5cb38a18323d700a3a7
levanhoangtsth1977-crypto <levanhoangtsth1977@gmail.com>
2026-08-31 19:37:59 +0700

    Add Master Editor root layout

---
+++ giao-an-master-editor/app/layout.js
@@ -0,0 +1,10 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Giáo án Master Editor 2026',
+  description: 'Công cụ kiểm tra và chuẩn hóa giáo án Word có kiểm soát thay đổi.'
+};
+
+export default function RootLayout({ children }) {
+  return <html lang="vi"><body>{children}</body></html>;
+}
